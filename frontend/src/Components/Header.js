cb5629e0c9b2b50cfb583a8839b7bc499d0a2858
hao.chen8 <hc222ig@student.lnu.se>
2024-04-16 22:32:49 +0200

    Begin footer

--- frontend/src/Components/Header.js
+++ frontend/src/Components/Header.js
@@ -1,11 +1,19 @@
+/**    
+ * The Header component of the website. 
+ *
+ * @author: Hao Chen
+ * @version: 1.0
+ */
+
 import React from 'react'
 import { Link, NavLink } from 'react-router-dom';
 import { BsSearch } from 'react-icons/bs'
 
+
 const Header = () => {
   return (
   <>
-    <header className='header-top-strip py-3'>
+    <header className='header-top-strip'>
       <div className="container-xxl">
         <div className="row">
           <div className="col-6">
@@ -78,13 +86,37 @@ const Header = () => {
       <div className="container-xxl">
         <div className="row">
           <div className="col-12">
-            <div className="menu-bottom d-flex align-items-center">
+            <div className="menu-bottom d-flex align-items-center gap-30">
+              <div>
+              <div className="dropdown">
+                <button className="btn btn-secondary dropdown-toggle bg-transparent border-0 gap-15 d-flex align-items-center" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                 <img src="images/menu_list.png" alt="menu" /> <span className='me-5 d-inline-block'>Categories</span>
+                </button>
+                <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                  <li>
+                    <Link className="dropdown-item text-white" to="">
+                      Action
+                      </Link>
+                      </li>
+                  <li>
+                    <Link className="dropdown-item text-white" to="">
+                      Another action
+                      </Link>
+                      </li>
+                  <li>
+                    <Link className="dropdown-item text-white" to="">
+                      Something else here
+                    </Link>
+                    </li>
+                </ul>
+              </div>
+              </div>
               <div className="menu-links">
-                <div className="d-flex align-items-center gap-15">
-                  <NavLink>Home</NavLink>
-                  <NavLink>New Arrivals</NavLink>
-                  <NavLink>Deals/Offers</NavLink>
-                  <NavLink>FAQ</NavLink>
+                <div className="navlinks d-flex align-items-center gap-15">
+                  <NavLink  to='/'>Home</NavLink>
+                  <NavLink  to='/'>New Arrivals</NavLink>
+                  <NavLink  to='/'>Deals/Offers</NavLink>
+                  <NavLink  to='/'>FAQ</NavLink>
 
                 </div>
               </div>
