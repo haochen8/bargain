4d4117da4cd18aab62010b159c5bddbbe9d0ceaa
hao.chen8 <hc222ig@student.lnu.se>
2024-05-06 12:35:39 +0200

    chore: Add product model

---
+++ src/models/productModel.js
@@ -0,0 +1,71 @@
+/**
+ * @file Defines the product model.
+ * @module models/productModel
+ * @author Hao Chen
+ * @version 3.1.0
+ */
+
+import mongoose from 'mongoose'
+import { BASE_SCHEMA } from './baseSchema.js'
+
+
+// The product schema.
+const productSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  slug: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Category',
+  },
+  brand: {
+    type: String,
+    enum: ['Apple', 'Samsung', 'Intel']
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+  sold: {
+    type: Number,
+    default: 0,
+  },
+  images: {
+   type: Array,
+  },
+  color: {
+    type: String,
+    enum: ['Black', 'White', 'Red', 'Blue', 'Green', 'Yellow', 'Purple', 'Orange', 'Pink', 'Brown', 'Gray'],
+  },
+  ratings: [{
+    star: Number,
+    postedBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
+  }],
+}, {
+  timestamps: true,
+})
+
+productSchema.add(BASE_SCHEMA)
+
+// Create a model using the schema.
+export const UserModel = mongoose.model('Product', productSchema)
