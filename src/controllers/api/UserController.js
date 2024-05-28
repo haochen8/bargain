553fc57e8995f9aa24caf39e6946fbed583a4ac1
hao.chen8 <hc222ig@student.lnu.se>
2024-05-28 19:12:27 +0200

    Fix secure,sameSite

--- src/controllers/api/UserController.js
+++ src/controllers/api/UserController.js
@@ -126,8 +126,8 @@ export class UserController {
       res.cookie("refreshToken", refreshToken, {
         httpOnly: true,
         maxAge: 24 * 60 * 60 * 1000,
-        sameSite: process.env.NODE_ENV === 'production', // Use secure cookies in production
-        secure: true,
+        sameSite: "none",
+        secure: process.env.NODE_ENV === 'production', // Use secure cookies in production
         path: "/",
       });
 
